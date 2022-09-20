5215d8267a8da21a8f53d49b94d8bdf3217fe531
Hoid2 <61083843+Hoid2@users.noreply.github.com>
2022-09-20 08:36:41 -0400

    Remove unused method and add TODO

--- src/main/java/megaminds/clickopener/ClickOpenerMod.java
+++ src/main/java/megaminds/clickopener/ClickOpenerMod.java
@@ -2,14 +2,13 @@ package megaminds.clickopener;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.minecraft.item.BlockItem;
-import net.minecraft.server.network.ServerPlayerEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import megaminds.clickopener.api.HandlerRegistry;
 import megaminds.clickopener.util.Config;
 
+//TODO Add duplication checking (if person takes shulkerbox out of chest while someone else is looking into it)
+//TODO Stop ItemStack from being able to be moved when it is opened
 public class ClickOpenerMod implements ModInitializer {
 	public static final String MODID = "clickopener";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
@@ -18,10 +17,5 @@ public class ClickOpenerMod implements ModInitializer {
 	public void onInitialize() {
 		Config.load();
 		CommandRegistrationCallback.EVENT.register(Commands::register);
-	}
-
-	public static void open(BlockItem item, ServerPlayerEntity player) {
-		var fac = HandlerRegistry.get(item).createFactory(null, player, null);
-		player.openHandledScreen(fac);
 	}
 }
