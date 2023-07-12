3eeab48b9b92f710082ca03a9b241365ba1f9423
Hoid2 <61083843+Hoid2@users.noreply.github.com>
2023-07-12 17:08:58 -0400

    Handle nulls in IdentifierAdapter

--- src/main/java/megaminds/clickopener/util/IdentifierAdapter.java
+++ src/main/java/megaminds/clickopener/util/IdentifierAdapter.java
@@ -2,20 +2,25 @@ package megaminds.clickopener.util;
 
 import java.io.IOException;
 import com.google.gson.TypeAdapter;
-import com.google.gson.internal.bind.TypeAdapters;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 
 import net.minecraft.util.Identifier;
 
 public class IdentifierAdapter extends TypeAdapter<Identifier> {
 	@Override
-	public Identifier read(JsonReader arg0) throws IOException {
-		return new Identifier(TypeAdapters.STRING.read(arg0));
+	public Identifier read(JsonReader in) throws IOException {
+		if (in.peek() == JsonToken.NULL) {
+			in.nextNull();
+			return null;
+		}
+
+		return new Identifier(in.nextString());
 	}
 
 	@Override
-	public void write(JsonWriter arg0, Identifier arg1) throws IOException {
-		TypeAdapters.STRING.write(arg0, arg1.toString());
+	public void write(JsonWriter out, Identifier id) throws IOException {
+		out.value(id == null ? null : id.toString());
 	}
 }
