018d95a8e831c011f2e83eb60d72b4018964b24b
Hoid2 <61083843+Hoid2@users.noreply.github.com>
2023-07-14 18:01:05 -0400

    Delegate screen opening to block.onUse
    -this only applies to some blocks
    -block stats should now apply
    -piglin angering should now apply

--- src/main/java/megaminds/clickopener/api/ItemScreenOpener.java
+++ src/main/java/megaminds/clickopener/api/ItemScreenOpener.java
@@ -1,21 +1,19 @@
 package megaminds.clickopener.api;
 
-import megaminds.clickopener.screenhandlers.AnvilItemScreenHandler;
-import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.NamedScreenHandlerFactory;
-import net.minecraft.screen.ScreenHandlerContext;
-import net.minecraft.screen.ShulkerBoxScreenHandler;
-import net.minecraft.screen.SimpleNamedScreenHandlerFactory;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
 
 public interface ItemScreenOpener {
+	@SuppressWarnings("deprecation")
+	public static final ItemScreenOpener BLOCK_USE_HANDLER = (stack, player, i) -> {
+		var block = ((BlockItem)stack.getItem()).getBlock();
+		return block.onUse(block.getDefaultState(), player.getServerWorld(), player.getBlockPos(), player, null, null) == ActionResult.CONSUME;
+	};
 	public static final ItemScreenOpener BLOCK_STATE_HANDLER = (ScreenFactoryOpener)(stack, player, i) -> ((BlockItem)stack.getItem()).getBlock().getDefaultState().createScreenHandlerFactory(player.getWorld(), player.getBlockPos());
-	public static final ItemScreenOpener ANVIL_HANDLER = (ScreenFactoryOpener)(stack, p, inv) -> new SimpleNamedScreenHandlerFactory((syncId, inventory, player)->new AnvilItemScreenHandler(syncId, inventory, ScreenHandlerContext.create(player.getWorld(), player.getBlockPos()), inv, stack), Text.translatable("container.repair"));
-	public static final ItemScreenOpener SHULKER_BOX_HANDLER = ItemScreenOpener.requireSingleStack((ScreenFactoryOpener)(stack, p, i)->new SimpleNamedScreenHandlerFactory((syncId, inventory, player)->new ShulkerBoxScreenHandler(syncId, inventory, new ShulkerInventory(stack, 27, BlockEntityType.SHULKER_BOX)), stack.hasCustomName() ? stack.getName() : Text.translatable("container.shulkerBox")));
 
 	boolean open(ItemStack stack, ServerPlayerEntity player, Inventory inventory);
 
@@ -26,9 +24,16 @@ public interface ItemScreenOpener {
 	public interface ScreenFactoryOpener extends ItemScreenOpener {
 		NamedScreenHandlerFactory createFactory(ItemStack stack, ServerPlayerEntity player, Inventory inventory);
 
+		@SuppressWarnings("unused")
+		default void afterSuccess(ItemStack stack, ServerPlayerEntity player, Inventory inventory) {}
+
 		@Override
 		default boolean open(ItemStack stack, ServerPlayerEntity player, Inventory inventory) {
-			return player.openHandledScreen(createFactory(stack, player, inventory)).isPresent();
+			var success = player.openHandledScreen(createFactory(stack, player, inventory)).isPresent();
+			if (success) {
+				afterSuccess(stack, player, inventory);
+			}
+			return success;
 		}
 	}
 }
